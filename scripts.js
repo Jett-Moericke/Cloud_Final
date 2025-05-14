08900fcd17cceae34075a67ca80b95dbaa5b943a
Jett-Moericke <Jettmoericke@gmail.com>
2025-05-14 11:06:29 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('showImageBtn').addEventListener('click', function() {
+  const photoContainer = document.getElementById('photoContainer');
+  photoContainer.classList.toggle('hidden');
+});
